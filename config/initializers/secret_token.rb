7a5c4584b919dc5f4a04355414ebef20a98acf2c
Shawn <gaffneyshawn23@gmail.com>
2011-10-17 15:03:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComputerLab::Application.config.secret_token = 'f365fbc61980ff0b6ac876b36f8c4bcb1c852a3c7ed068b9f91365eeaafa9403527be8a7cff8dca85d31d2492e4aec652767c0d5a007d588a976f8cbcdcb53cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ComputerLab::Application.config.session_store :cookie_store, :key => '_computer_lab_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ComputerLab::Application.config.session_store :active_record_store
